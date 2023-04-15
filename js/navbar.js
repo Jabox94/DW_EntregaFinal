2e762ce70938cab40766b292ce2242b017000ece
Javier Miranda <Jabox2002@proton.me>
2023-04-15 02:56:35 -0300

    Arreglando errores menoresgi

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,10 @@
 const nav = document.querySelector('.menu__button');
 const action = document.querySelector('.nav__navbar');
+const form = document.querySelector('.form-label');
 
 
 nav.addEventListener('click', () => {
     action.classList.toggle('nav__navbar--active');
     nav.classList.toggle('menu__button--active');
+    form.classList.toggle('text-white');
 })
